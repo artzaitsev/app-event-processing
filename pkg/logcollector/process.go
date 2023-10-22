f006d01bba2a9778488ff2471cba731f7fcfb684
artzaycev <mail@artzaycev.ru>
2023-10-22 23:31:50 +0300

    processing

--- pkg/logcollector/process.go
+++ pkg/logcollector/process.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"sync"
 )
 
 var (
@@ -15,29 +16,37 @@ var (
 func Process[T any](
 	ctx context.Context,
 	reader io.Reader,
-	batchSiz int,
+	batchSize int,
 	saveFunc func(ctx context.Context, batch []T) error,
 ) error {
-	if batchSiz < 1 {
-		batchSiz = 50
+	if batchSize < 1 {
+		batchSize = 50
 	}
 
-	batches, err := split[T](reader, batchSiz)
+	batches, err := split[T](reader, batchSize)
 	if err != nil {
-		return errors.Join(ErrMakeBatch, err)
+		return fmt.Errorf("%w: %w", ErrMakeBatch, err)
 	}
 
+	var wg sync.WaitGroup
 	results := make(chan error, len(batches))
+
 	for _, batch := range batches {
+		wg.Add(1)
 		go func(ctx context.Context, b []T, r chan<- error) {
+			defer wg.Done()
 			err := saveFunc(ctx, b)
 			r <- err
 		}(ctx, batch, results)
 	}
 
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
 	var processError error
-	for i := 0; i < len(batches); i++ {
-		err = <-results
+	for err := range results {
 		if err != nil {
 			processError = fmt.Errorf("%w: %w", ErrProcessing, err)
 		}
